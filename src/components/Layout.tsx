4634505a0687e705c18427402cca81c0f0045bec
chanchaljaiswal12345 <chanchal.jaiswal@stackinfinite.com>
2023-08-25 14:42:22 +0530

    dynamic web page

---
+++ src/components/Layout.tsx
@@ -0,0 +1,12 @@
+import React, { PropsWithChildren } from "react";
+import Navbar from "./navbar";
+
+const Layout = ({ children }: PropsWithChildren) => {
+  return (
+    <>
+      <Navbar/>
+      {children}
+    </>
+  );
+};
+export default Layout;
---
+++ src/pages/services.tsx
@@ -0,0 +1,33 @@
+export default function Services() {
+  return (
+    <>
+      <section id="blog" className=" text-white h-screen bg-green-900 flex">
+        <h1 className="m-auto mt-2 font-waterfall text-7xl text-white font-bold">
+          Services Page
+        </h1>
+
+        <p>
+          His life as a man is built around health insurance and tax services.
+          HOUELLEBECQ’S INCENDIARY NOVEL IMAGINES FRANCE WITH A MUSLIM PRESIDENT
+          | PIERRE ASSOULINE | JANUARY 9, 2015 | THE DAILY BEAST “We met the
+          smuggler in the train station; he came to speak with us about the
+          services he provided,” Yazbek says. GHOST SHIPS OF THE MEDITERRANEAN |
+          BARBIE LATZA NADEAU | JANUARY 6, 2015 | THE DAILY BEAST Users “should
+          be allowed to use these devices and services the way they were
+          intended,” Brookman says. HOW ‘ETHICAL’ HOTEL CHAIN MARRIOTT GOUGES
+          GUESTS IN THE NAME OF WI-FI SECURITY | KYLE CHAYKA | DECEMBER 31, 2014
+          | THE DAILY BEAST services like Airbnb, Yelp, and yes, Uber are
+          disrupting long-established industries, from taxis to hotels. WHY DO
+          ‘PROGRESSIVES’ WANT TO BAN UBER AND AIRBNB? | ADAM THIERER,
+          CHRISTOPHER KOOPMAN | DECEMBER 30, 2014 | THE DAILY BEAST “He
+          literally went underground to hold services,” Moscow-based dissident
+          and journalist Victor Davidoff said in an email. REMEMBERING THE
+          RUSSIAN PRIEST WHO FOUGHT THE ORTHODOX CHURCH | CATHY YOUNG | DECEMBER
+          28, 2014 | THE DAILY BEAST
+        </p>
+      </section>
+
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg"></div>
+    </>
+  );
+}
